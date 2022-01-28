d82e7ebcf901d291fc72a9c1bef2c327590ea973
John William Davis <jwdavisdeveloper@gmail.com>
2022-01-27 23:20:24 -0800

    Create first form component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import AddFamilyMember from "./components/AddFamilyMember/AddFamilyMember.js";
+
 function App() {
   return (
-    <div className='bg-amber-200 h-screen m-0'>
+    <div
+      className='bg-amber-200 h-screen m-0 flex flex-col items-center
+    '
+    >
       <h1 className='text-3xl font-bold text-center py-5'>Mi Familia</h1>
+      <AddFamilyMember />
     </div>
   );
 }
--- src/components/AddFamilyMember/AddFamilyMember.js
+++ src/components/AddFamilyMember/AddFamilyMember.js
@@ -1,7 +1,43 @@
 import React from "react";
 
 const AddFamilyMember = () => {
-  return <div />;
+  return (
+    <div className='rounded-md flex flex-col bg-amber-600 text-amber-100 text-2xl w-2/5 px-6 py-8'>
+      <h3 className='text-center text-4xl mb-3 font-bold'>Family Member</h3>
+      <form className='flex flex-col items-center'>
+        <div className='flex flex-col items-center'>
+          <label htmlFor='name' className='py-3 self-start'>
+            Name
+          </label>
+          <input type='text' name='name' id='name' />
+        </div>
+        <div className='flex flex-col items-center'>
+          <label htmlFor='age' className='py-3 self-start'>
+            Age
+          </label>
+          <input type='number' name='age' id='age' />
+        </div>
+        <div className='flex flex-col items-center'>
+          <label htmlFor='color' className='py-3 self-start'>
+            Favorite Color
+          </label>
+          <input type='text' name='color' id='color' />
+        </div>
+        <div className='flex flex-col items-center'>
+          <label htmlFor='month' className='py-3 self-start'>
+            Birth Month
+          </label>
+          <input type='text' name='month' id='month' />
+        </div>
+        <button
+          type='submit'
+          className='rounded px-4 py-3 mt-8  mb-4 border-4 border-amber-50 text-amber-50 bg-amber-500'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddFamilyMember;

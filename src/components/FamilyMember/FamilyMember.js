6d7c4f0e76f9557ed731112826479485727a9111
John William Davis <jwdavisdeveloper@gmail.com>
2022-01-30 15:01:22 -0800

    Build out familyMember component - implement spanish - tailwind styles

--- src/components/FamilyMember/FamilyMember.js
+++ src/components/FamilyMember/FamilyMember.js
@@ -1,15 +1,29 @@
 import { useContext } from "react";
 import SpanishContext from "../../Context/SpanishContext.js";
 
-const FamilyMember = ({ id, name, color, age, favorite }) => {
+const FamilyMember = ({ id, name, color, age, favorite, month }) => {
   const { spanish } = useContext(SpanishContext);
   return (
-    <div key={id}>
-      <h2>{spanish ? "El amado:" : "The Loved One:"}</h2>
-      <h3>{name}</h3>
-      <h3>Favorite Color is: {color}</h3>
-      <h3>Age is: {age}</h3>
-      <h3>Favorite activity is: {favorite}</h3>
+    <div key={id} className='flex flex-col w-full p-6 bg-amber-300 mt-4 text-amber-700'>
+      <h2 className='text-center font-bold text-xl pb-3'>
+        {spanish ? "El amado" : "The Loved One"}
+      </h2>
+      <p>
+        {spanish ? "Nombre:" : "Name:"} <span className='font-bold'>{name}</span>
+      </p>
+      <p>
+        {spanish ? "Color favorito:" : "Favorite Color:"} <span className='font-bold'>{color}</span>
+      </p>
+      <p>
+        {spanish ? "Edad:" : "Age:"} <span className='font-bold'>{age}</span>
+      </p>
+      <p>
+        {spanish ? "Mes de nacimiento" : "Birth Month:"} <span className='font-bold'>{month}</span>
+      </p>
+      <p>
+        {spanish ? "Actividad favorito" : "Favorite Activity:"}{" "}
+        <span className='font-bold'>{favorite}</span>
+      </p>
     </div>
   );
 };
--- src/components/ListFamily/ListFamily.js
+++ src/components/ListFamily/ListFamily.js
@@ -5,12 +5,16 @@ import FamilyMember from "../FamilyMember/FamilyMember.js";
 const ListFamily = ({ family }) => {
   const { spanish } = useContext(SpanishContext);
   return (
-    <div className='flex flex-col'>
-      <h1>{!spanish ? "The People I Love" : "La gente que amo"}</h1>
-      {family.map((member) => {
-        return <FamilyMember {...member} />;
-      })}
-    </div>
+    <section className='rounded-lg flex flex-col md:w-1/2 bg-amber-800 text-amber-100 p-6'>
+      <h1 className='text-center text-xl font-bold'>
+        {!spanish ? "The People I Love" : "La gente que amo"}
+      </h1>
+      <div>
+        {family.map((member) => {
+          return <FamilyMember {...member} />;
+        })}
+      </div>
+    </section>
   );
 };
 
